082f401e23e683b583f10fdb80ff2b47a6e3577a
sandoreno <ayazkhas@mail.ru>
2023-05-18 23:37:24 +0300

    add bootstrap and routing components

--- Task19UI/src/app/app-routing.module.ts
+++ Task19UI/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CatalogPageComponent } from './components/catalog-page/catalog-page.component';
+import { StartPageComponent } from './components/start-page/start-page.component';
+import { TestsPageComponent } from './components/tests-page/tests-page.component';
+
+const routes: Routes = [
+  {path : 'catalog', component : CatalogPageComponent},
+  {path : 'dashboard', component : StartPageComponent},
+  {path : 'tests', component : TestsPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
