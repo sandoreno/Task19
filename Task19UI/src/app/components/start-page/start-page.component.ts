a2fbb253bbb683d7dd40f5d0011cbf4472df2fec
Arzhan <chilchinov99@mail.ru>
2023-05-20 13:35:05 +0300

    set up global style, rework start_page, add components on start page

--- Task19UI/src/app/components/start-page/start-page.component.ts
+++ Task19UI/src/app/components/start-page/start-page.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./start-page.component.scss']
 })
 export class StartPageComponent {
-
+  events_education = [
+    { name: ["Пеший лекторий","Английскай язык","Шахматы и шашки"] ,
+     status: ["","ОНЛАЙН"] },
+  ];
+  events_heal = [
+    { name: ["Скандинаская ходьба", "Суставная гимнастика", "Оздоровительная гимнастика"] ,
+     status: ["","ОНЛАЙН"] },
+  ];
+  events_creative = [
+    { name: ["Мастер-класс по уходу за кожей в зрелом возрасте", "Рисование", "Пение"] ,
+     status: ["","ОНЛАЙН"] },
+  ];
+  events_specially = [
+    { name: ["Московский театрал", "Секреты добрососедства","Психологические тренинги"] ,
+     status: ["","ОНЛАЙН"] },
+  ];
 }
