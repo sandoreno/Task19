9b27d814e186ff1155bb9f3f5b2663c0674be046
dev_danil <tinder12347@mail.ru>
2023-05-21 19:35:08 +0300

    add DTOs to CORE

--- Task19API/Task19API/Models/Scrobble.cs
+++ Task19API/Task19API/Models/Scrobble.cs
@@ -6,12 +6,8 @@ namespace Task19API.Models;
 public partial class Scrobble
 {
     public int? UserId { get; set; }
-
     public int? GroupId { get; set; }
-
     public int? Scrobbles { get; set; }
-
-    public virtual Group? Group { get; set; }
-
-    public virtual User? User { get; set; }
+    public Group? Group { get; set; }
+    public User? User { get; set; }
 }
--- Task19API/Task19API/Models/User.cs
+++ Task19API/Task19API/Models/User.cs
@@ -6,12 +6,8 @@ namespace Task19API.Models;
 public partial class User
 {
     public int UniqueNumber { get; set; }
-
     public DateTime? DateCreated { get; set; }
-
     public string? Sex { get; set; }
-
     public DateOnly? Birthdate { get; set; }
-
     public string? Address { get; set; }
 }
