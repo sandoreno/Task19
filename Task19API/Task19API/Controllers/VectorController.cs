a8c417d8b6d75aeb32d68686041f2ca8d40f0c64
dev_danil <tinder12347@mail.ru>
2023-05-27 00:37:14 +0300

    create Vector

---
+++ Task19API/Task19API/Controllers/VectorController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Task19API.DTOs;
+using Task19API.Interface;
+
+namespace Task19API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VectorController : ControllerBase
+    {
+        private readonly IVector _vector;
+
+        public VectorController(IVector vector)
+        {
+            _vector = vector;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<int>>> CreateVector(TestModelResponse model)
+        {
+            try
+            {
+                var vector = await _vector.Vector(model);
+                
+                return vector;
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ Task19API/Task19API/Interface/IVector.cs
@@ -0,0 +1,9 @@
+﻿using Task19API.DTOs;
+
+namespace Task19API.Interface
+{
+    public interface IVector
+    {
+        public Task<List<int>> Vector(TestModelResponse model);
+    }
+}
